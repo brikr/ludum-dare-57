f13e98c3029b460c23d8cdf98017bf49b9bda715
Brik <brikr@users.noreply.github.com>
2025-04-18 22:22:43 -0700

    jetpack physics

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
+# max horizontal ground speed
 var speed = 150.0
+# acceleration (per physics frame)
 var accel = 30.0
+# acceleration in the air
 var air_accel = 5.0
-var jump_velocity = 310.0
+# instantaneous vertical velocity bonus when jumping off the ground
+var jump_velocity = 300.0
+# jetpack vertical acceleration
+var jetpack_accel = 30.0
+# max upward speed
+var jetpack_speed_limit = 175.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# max fall speed
+var terminal_velocity = 350.0
 
 
 func _physics_process(delta):
@@ -14,6 +24,7 @@ func _physics_process(delta):
     # Handle jump
     if Input.is_action_just_pressed("jump"):
       velocity.y = jump_velocity * -1
+      $JetpackDelay.start()
 
     # Handle floor movement
     var move_direction = Input.get_axis("move_left", "move_right")
@@ -25,6 +36,8 @@ func _physics_process(delta):
     # Air stuff
     # Apply gravity
     velocity.y += gravity * delta
+    # limit fall speed
+    velocity.y = min(velocity.y, terminal_velocity)
     # Handle air movement
     var move_direction = Input.get_axis("move_left", "move_right")
     if move_direction:
@@ -32,10 +45,14 @@ func _physics_process(delta):
     else:
       velocity.x = move_toward(velocity.x, 0, air_accel)
 
+    if Input.is_action_pressed("jump") and $JetpackDelay.is_stopped():
+      # jetpack go brrr
+      velocity.y -= jetpack_accel
+      # limit jetpack boosties
+      velocity.y = max(velocity.y, -jetpack_speed_limit)
 
   move_and_slide()
 
 func _process(delta: float) -> void:
   if Input.is_action_pressed("primary_mouse_action"):
     pass
-    
