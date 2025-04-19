b3c23c15499f2224623007e5328b2c3f1dae6891
Brik <brikr@users.noreply.github.com>
2025-04-18 22:39:21 -0700

    new dirt background tile in tileset and skybox matches width of world

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,5 +1,9 @@
 extends Node
 
+func _ready():
+  # make the skybox match the width of the generated world
+  $Sky.size.x = Constants.MAX_WORLD_WIDTH * Constants.TILE_WIDTH
+
 func _process(_delta):
   if Input.is_action_just_pressed("ui_cancel"):
     get_tree().quit()
