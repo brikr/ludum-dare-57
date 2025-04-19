ff29be701edaf923ee5511bebf6077f8197044cd
Alex Inskeep <inskeep.alex@gmail.com>
2025-04-19 12:50:37 -0600

    world gen refinement

--- scenes/game_state/game_state.gd
+++ scenes/game_state/game_state.gd
@@ -19,21 +19,30 @@ func is_edge(x, y):
   return is_horizontal_edge(x) || is_vertical_edge(y)
 
 func gen_under_world():
-  gen_base_under_world()
-  gen_resource(Tile.TileObjectType.GOLD)
+  gen_base_under_world() # generates dirt and borders
+
   gen_resource(Tile.TileObjectType.BLUE)
+  gen_resource(Tile.TileObjectType.GOLD)
 
 func gen_resource(resourceType: Tile.TileObjectType):
   var noise = GenUtils.get_noise()
   var min_depth = Constants.WORLD_GEN[resourceType][Constants.WORLD_GEN_FIELDS.MIN_DEPTH]
   var max_depth = Constants.WORLD_GEN[resourceType][Constants.WORLD_GEN_FIELDS.MAX_DEPTH]
-  var depth_coefficient = Constants.WORLD_GEN[resourceType][Constants.WORLD_GEN_FIELDS.COEFFICIENT]
+  var min_depth_coefficient = Constants.WORLD_GEN[resourceType][Constants.WORLD_GEN_FIELDS.MIN_COEFFICIENT]
+  var max_depth_coefficient = Constants.WORLD_GEN[resourceType][Constants.WORLD_GEN_FIELDS.MAX_COEFFICIENT]
   var threashold = Constants.WORLD_GEN[resourceType][Constants.WORLD_GEN_FIELDS.THRESHOLD]
 
   for x in Constants.MAX_WORLD_WIDTH:
     for y in range(Constants.SURFACE_HEIGHT + min_depth, max_depth):
       var coords := Vector2i(x, y)
+
+      # How far are we from min -> max depth (normalized 0.0 to 1.0)
+      var depth_ratio := float(y - (Constants.SURFACE_HEIGHT + min_depth)) / float(max_depth - (Constants.SURFACE_HEIGHT + min_depth))
+      # Sweep coefficient based on depth.
+      var depth_coefficient = lerp(min_depth_coefficient, max_depth_coefficient, depth_ratio)
+
       var probability = noise.get_noise_2dv(coords) * y * depth_coefficient
+      print(coords, probability)
       if probability > threashold && map[coords].type == Tile.TileType.DIRT:
         map[coords].objectType = resourceType
 
