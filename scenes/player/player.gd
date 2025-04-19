ebad1fe0f5615fc7ff1bf35162948e0d353de2e7
Alex Inskeep <inskeep.alex@gmail.com>
2025-04-19 15:51:46 -0600

    fix typo

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -83,7 +83,11 @@ func _animate_on_ground(velocity: Vector2):
 func _process_movement(delta):
   # ainskeep tesing
   if Input.is_action_just_pressed("secondary_mouse_action"):
-    Item.new(Item.ItemType.GEAR, {Item.StatImpact.CURRENT_FUEL: 5}).apply(self)
+    Item.new(Item.ItemType.GEAR, {
+      Item.StatImpact.CURRENT_FUEL: 100,
+      Item.StatImpact.MAX_JETPACK_ACCEL: 1000,
+      Item.StatImpact.JETPACK_SPEED_LIMIT: 1000
+    }).apply(self)
 
   if is_on_floor():
     # Ground stuff
--- scripts/items/item.gd
+++ scripts/items/item.gd
@@ -1,7 +1,16 @@
 class_name Item extends Node
 
 enum ItemType {GEAR, CONSUMABLE}
-enum StatImpact {CURRENT_FUEL, MAX_JETPACK_ACCEL}
+enum StatImpact {
+  FUEL_CAPACITY,
+  MAX_JETPACK_ACCEL,
+  MIN_JETPACK_ACCEL,
+  JETPACK_ACCEL_PENALTY,
+  JETPACK_SPEED_LIMIT,
+  JETPACK_FUEL_EFFICIENCY,
+  CURRENT_FUEL,
+  DIGGING_POWER,
+}
 
 var itemType: ItemType
 var itemStatImpact: Dictionary[StatImpact, float]
@@ -14,9 +23,19 @@ func apply(player) -> void:
   for key in itemStatImpact:
     var val = itemStatImpact[key]
     match key:
-      Item.StatImpact.CURRENT_FUEL:
-        player.current_fuel += val
+      Item.StatImpact.FUEL_CAPACITY:
+        player.fuel_capacity += val
       Item.StatImpact.MAX_JETPACK_ACCEL:
         player.max_jetpack_accel += val
-      _:
-        pass
+      Item.StatImpact.MIN_JETPACK_ACCEL:
+        player.min_jetpack_accel += val
+      Item.StatImpact.JETPACK_ACCEL_PENALTY:
+        player.jetpack_accel_penalty += val
+      Item.StatImpact.JETPACK_SPEED_LIMIT:
+        player.jetpack_speed_limit += val
+      Item.StatImpact.JETPACK_FUEL_EFFICIENCY:
+        player.jetpack_fuel_efficiency += val
+      Item.StatImpact.CURRENT_FUEL:
+        player.current_fuel += val
+      Item.StatImpact.DIGGING_POWER:
+        player.digging_power += val
