26f5513fa6f15b4d9da263a6260e57e52690920d
Alex Inskeep <inskeep.alex@gmail.com>
2025-04-20 15:41:55 -0600

    add wood

--- scripts/items/items.gd
+++ scripts/items/items.gd
@@ -6,7 +6,8 @@ var GEAR_REGISTRY = [
   Item.new("Overdrive Rotor", Item.Category.DRILL, 1000.0, Item.Type.GEAR, {Item.StatImpact.DIGGING_POWER: 0.5}),
   Item.new("Worldbreaker Bit", Item.Category.DRILL, 5000.0, Item.Type.GEAR, {Item.StatImpact.DIGGING_POWER: 1.0}),
   ## Heat
-  Item.new("Aluminum Heat Sink", Item.Category.HEAT, 100.0, Item.Type.GEAR, {Item.StatImpact.HEAT_CAPACITY: 100.0, Item.StatImpact.HEAT_DECAY_RATE: 0.5}),
+  Item.new("Wood Heat Sink", Item.Category.HEAT, 50.0, Item.Type.GEAR, {Item.StatImpact.HEAT_CAPACITY: 50.0}),
+  Item.new("Aluminum Heat Sink", Item.Category.HEAT, 500.0, Item.Type.GEAR, {Item.StatImpact.HEAT_CAPACITY: 100.0, Item.StatImpact.HEAT_DECAY_RATE: 0.5}),
   Item.new("Cryocooler", Item.Category.HEAT, 5000.0, Item.Type.GEAR, {Item.StatImpact.DIGGING_HEAT_GEN: -0.2, Item.StatImpact.HEAT_DECAY_RATE: 0.5}),
   ## Fuel
   Item.new("Auxiliary Fuel Tank", Item.Category.FUEL, 200.0, Item.Type.GEAR, {Item.StatImpact.FUEL_CAPACITY: 1000}),
