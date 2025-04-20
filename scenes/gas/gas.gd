9dad7e0318401a88215e1831325aed0314caaafd
Brik <brikr@users.noreply.github.com>
2025-04-20 11:35:44 -0700

    movema gas

--- scenes/gas/gas.gd
+++ scenes/gas/gas.gd
@@ -5,11 +5,13 @@ var player_present = false
 func _process(delta):
   if Input.is_action_just_pressed("interact") && player_present:
     GameState.player.refuel()
+    $FGlyph.visible = false
 
 func _on_player_entered(body: Node2D) -> void:
   player_present = true
-  $FGlyph.visible = true
-  $FGlyph.play()
+  if GameState.player.current_fuel < GameState.player.fuel_capacity:
+    $FGlyph.visible = true
+    $FGlyph.play()
 
 func _on_player_exited(body: Node2D) -> void:
   player_present = false
